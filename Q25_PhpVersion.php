0c452cdab89e6062fa46775f80e3e8bfd7d56bb5
vansh1027 <vanshthkr.72011027@gmail.com>
2025-02-08 10:21:02 +0530

    Q25_PhpVersion.php

--- Q25_PhpVersion.php
+++ Q25_PhpVersion.php
@@ -1,5 +1,5 @@
 <?php
 	echo "<h1>PHP Version Info</h1>";
 	echo "Current version of php: ". phpversion();
-	echo "<br><br> This program is executed by Kamal Mittal!";
+	echo "<br><br> This program is executed by vansh thakur,2220100302";
 ?>
