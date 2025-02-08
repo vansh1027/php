d00fd54416b2c2551dca59c5d3ca12b9ad8de13f
vansh1027 <vanshthkr.72011027@gmail.com>
2025-02-08 10:22:16 +0530

    Q29_Factorial.php

--- Q29_Factorial.php
+++ Q29_Factorial.php
@@ -7,5 +7,5 @@ for ($i = 1; $i <= $number; $i++) {
     $factorial *= $i;
 }
 echo "The factorial of $number is: $factorial\n";
-echo "This code is executed by Kamal Mittal!";
+echo "This code is executed by vansh thakur,2220100302";
 ?>
