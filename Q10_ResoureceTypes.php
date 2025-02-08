fb9caac8a55ae01132abdf9d08b34cb444e8101b
vansh1027 <vanshthkr.72011027@gmail.com>
2025-02-08 10:11:14 +0530

    Update Q10_ResoureceTypes.php

--- Q10_ResoureceTypes.php
+++ Q10_ResoureceTypes.php
@@ -1,5 +1,5 @@
 <?php
 	$handle = fopen("note.txt", "r");
 	var_dump($handle);
-	echo "<br> This code is executed by Kamal Mittal!";
+	echo "<br> This code is executed by vansh thakur,2220100302";
 ?>
