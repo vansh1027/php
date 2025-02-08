b1809e4b94bdab5962bb8ab21605e0a691cd655e
vansh1027 <vanshthkr.72011027@gmail.com>
2025-02-08 10:22:00 +0530

    Q28_SquareNumber.php

--- Q28_SquareNumber.php
+++ Q28_SquareNumber.php
@@ -4,5 +4,5 @@ $number = 5;
 $square = $number * $number;
 
 echo "The square of $number is: $square\n";
-echo "This code is executed by Kamal Mittal!";
+echo "This code is executed by vansh thakur,2220100302";
 ?>
